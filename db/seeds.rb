fa10ce9700c951e884888c68bc6c0b7f6563e162
Rachelik <rachel.ik.87@gmail.com>
2020-05-19 15:53:38 +0800

    Added seed teachers and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create :name => 'John Doe', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+teacher = Teacher.create :name => 'Tan Do', :email => 'tando@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+teacher = Teacher.create :name => 'Chen Do', :email => 'Chendo@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+project = Project.create :subject => 'History', :name => 'History Project', :teacher_id => 1
