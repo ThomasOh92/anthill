07fc1e3b9a52d16e2b36e172b08fa03c8a220a74
mdazharaw <darkaz62@hotmail.com>
2020-05-21 08:26:33 +0800

    Cleaned up sign in and sign up

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,5 +2,6 @@ class Teacher < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable, :trackable, :lockable 
+         :recoverable, :validatable, :trackable
+         #, :lockable 
 end
