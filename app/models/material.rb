c08f1c011f2f5f000a70e649dddff876d9fe9d2f
Rachelik <rachel.ik.87@gmail.com>
2020-05-25 14:20:25 +0800

    materials validation and teacher access

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,21 @@
 class Material < ApplicationRecord
   belongs_to :project
   has_one_attached :photo
+
+  validate :image_validation
+
+  def image_validation
+    if photo.attached?
+      if !photo.blob.content_type.in?(%w(photo/jpeg photo/jpg photo/png))
+        photo.purge_later
+        errors.add(:photo, 'The photo wrong format')
+      elsif photo.blob.content_type.in?(%w(photo/jpeg photo/jpg photo/png)) && photo.blob.byte_size > (5 * 1024 * 1024) # Limit size 5MB
+        photo.purge_later
+        errors.add(:photo, 'The photo oversize limited (5MB)')
+      end
+    elsif photo.attached? == false
+      photo.purge_later
+      errors.add(:photo, 'The photo required.')
+    end
+  end
 end
